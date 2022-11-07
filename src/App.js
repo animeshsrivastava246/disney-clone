fcad6b66c4bbd2e0996858cac7e0e6170ef8c10b
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2022-11-07 17:14:17 +0530

    Add Logos Image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World!</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login/>}>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
