06f13b8127ded72b86e1eb8352d0471c32431208
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2022-11-08 22:00:49 +0530

    Add Home NavMenu Icon

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,15 @@ import styled from "styled-components";
 const Login = (props) => {
     return (
         <Container>
-            <Content><BgImage/></Content>
+            <Content>
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg" alt="This is an image"/>
+                    <SignUp>JOIN</SignUp>
+                    <Discription>Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.</Discription>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt="This is an image"/>
+                </CTA>
+                <BgImage/>
+            </Content>
         </Container>
     )
 };
@@ -41,6 +49,53 @@ const BgImage = styled.div`
     right: 0;
     left: 0;
     z-index: -1;
+`;
+
+const CTA = styled.div`
+    max-width: 650px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const CTALogoOne = styled.img`
+    margin-bottom: 12px;
+    max-width: 600px;
+    min-width: 1px;
+    display: block;
+    width: 100%;
+`;
+
+const SignUp = styled.a`
+font-weight: bold;
+color: #f9f9f9;
+background-color: #0063e5;
+margin-bottom: 12px;
+width: 100%;
+letter-spacing: 1.5px;
+font-size: 18px;
+padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    &:hover {
+        background-color: #0483ee;
+    }
+`;
+
+const Discription = styled.p`
+color: hsla(0, 0%, 95.3%, 1);
+font-size: 11px;
+margin: 0 0 24px;
+line-height: 1.5;
+letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+    margin-bottom: 20px;
+    max-width: 600px;
+    vertical-align: bottom;
+    display: inline-block;
+    width: 100%;
 `;
 
 export default Login;
