e6aa5ed78fcba57ca77f3526e8d96b68744f7fc3
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2022-11-28 21:15:19 +0530

    Stuff 'n' Things+db.collection not function-Fix It

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 
 export default configureStore({
     reducer: {
         user: userReducer,
+        movie: movieReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
