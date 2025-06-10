f7388b98cde92155b4ae1414de21003f92a687c9
ANIMESH SRIVASTAVA <76392905+animeshsrivastava246@users.noreply.github.com>
2025-06-10 14:22:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import './App.css';
 import Login from "./components/Login";
 import Header from "./components/Header";
 import Home from "./components/Home";
