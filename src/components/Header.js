817eb70e2d1128422c030334c04020aa1ca44c80
ANIMESH SRIVASTAVA <76392905+animeshsrivastava246@users.noreply.github.com>
2025-06-25 14:54:49 +0530

    Update Header.js alt tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,23 +64,23 @@ const Header = (props) => {
                             <span>HOME</span>
                         </a>
                         <a href="/home" >
-                            <img src="/images/search-icon.svg" alt="HOME" />
+                            <img src="/images/search-icon.svg" alt="SEARCH" />
                             <span>SEARCH</span>
                         </a>
                         <a href="/home" >
-                            <img src="/images/watchlist-icon.svg" alt="HOME" />
+                            <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
                             <span>WATCHLIST</span>
                         </a>
                         <a href="/home" >
-                            <img src="/images/original-icon.svg" alt="HOME" />
+                            <img src="/images/original-icon.svg" alt="ORIGINALS" />
                             <span>ORIGINALS</span>
                         </a>
                         <a href="/home" >
-                            <img src="/images/movie-icon.svg" alt="HOME" />
+                            <img src="/images/movie-icon.svg" alt="MOVIES" />
                             <span>MOVIES</span>
                         </a>
                         <a href="/home" >
-                            <img src="/images/series-icon.svg" alt="HOME" />
+                            <img src="/images/series-icon.svg" alt="SERIES" />
                             <span>SERIES</span>
                         </a>
                     </NavMenu>
